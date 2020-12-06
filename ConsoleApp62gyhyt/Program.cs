f4211a47f5fbf82e385fa36a3538f5b5c223efb7
Nodar Nanishvili <nnanishvili7@gmail.com>
2020-12-06 16:45:53 +0200

    My first commit

---
+++ ConsoleApp62gyhyt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp62gyhyt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This work! Version 1.6");
+            Console.ReadKey();
+        }
+    }
+}
